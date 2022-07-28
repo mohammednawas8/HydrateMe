ba9fd3b562c931610785e790f73eb049c24f21b5
mohammad <mohammednawas8@gmail.com>
2022-07-28 16:55:07 +0300

    Status bar

--- app/src/main/java/com/example/hydrateme/hydrateme/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hydrateme/hydrateme/presentation/MainActivity.kt
@@ -9,17 +9,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.example.hydrateme.ui.theme.HydrateMeTheme
 import com.example.hydrateme.R
 import com.example.hydrateme.hydrateme.presentation.app_start_screens.GenderScreen.GenderScreen
 import com.example.hydrateme.hydrateme.presentation.app_start_screens.introduction_screen.IntroductionScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val systemUiController = rememberSystemUiController()
+                systemUiController.setSystemBarsColor(color = Color.White)
             HydrateMeTheme {
 
                 Surface(
